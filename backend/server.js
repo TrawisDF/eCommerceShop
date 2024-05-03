e67cebb3130eb483e207d7fe0cfa4a273f3dc068
TrawisDF <134593322+TrawisDF@users.noreply.github.com>
2024-05-03 13:00:00 +0700

    before deployment

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,14 @@
-import path from "path";
-import express from "express";
-import cookieParser from "cookie-parser";
-import products from "./data/products.js";
+import path from 'path';
+import express from 'express';
+import cookieParser from 'cookie-parser';
 const port = process.env.PORT || 5000;
-import productRoutes from "./routes/productRoutes.js";
-import usersRoutes from "./routes/userRoute.js";
-import connectDB from "./config/db.js";
-import dotenv from "dotenv";
-import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
-import ordersRoutes from "./routes/ordersRoutes.js";
-import uploadRoutes from "./routes/uploadRoutes.js";
+import productRoutes from './routes/productRoutes.js';
+import usersRoutes from './routes/userRoute.js';
+import connectDB from './config/db.js';
+import dotenv from 'dotenv';
+import { errorHandler, notFound } from './middleware/errorMiddleware.js';
+import ordersRoutes from './routes/ordersRoutes.js';
+import uploadRoutes from './routes/uploadRoutes.js';
 
 dotenv.config();
 connectDB();
@@ -24,25 +23,25 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const __dirname = path.resolve();
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-app.use("/api/products", productRoutes);
-app.use("/api/users", usersRoutes);
-app.use("/api/orders", ordersRoutes);
-app.use("/api/upload", uploadRoutes);
-app.get("/api/config/paypal", (req, res) =>
+app.use('/api/products', productRoutes);
+app.use('/api/users', usersRoutes);
+app.use('/api/orders', ordersRoutes);
+app.use('/api/upload', uploadRoutes);
+app.get('/api/config/paypal', (req, res) =>
   res.send({ clientId: process.env.PAYPAL_CLIENT_ID })
 );
 
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === 'production') {
   //set static folder
-  app.use(express.static(path.join(__dirname, "frontend/build")));
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  app.use(express.static(path.join(__dirname, 'frontend/build')));
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
   );
 } else {
-  app.get("/", (req, res) => {
-    res.send("api is running");
+  app.get('/', (req, res) => {
+    res.send('api is running');
   });
 }
 // Add this middleware to serve static files
